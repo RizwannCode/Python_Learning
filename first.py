7ae8b619f90b536dd92c5442008f7f05faba3f45
Rizwan Ali <129224488+RizwannCode@users.noreply.github.com>
2026-07-16 18:09:40 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,8 @@
+print("Hello world")
+
+
+a=5
+print(a)
+
+instructor = 'rishab mishra'
+print("python by", instructor , sep= '-')
---
+++ variables.py
@@ -0,0 +1,43 @@
+#variables in python
+a = 1
+print(a)
+
+b=1.3453
+print(b)
+
+c="Rizwan"
+print(a)
+
+rizwan= 34
+d=rizwan
+print(d)
+
+x=19
+print(x+1)
+
+p,q,r=12,13, 14
+print(p,q,r)
+
+# Ways to declare variables
+MyName = "rizwan" #pascal case
+myName = "rizwan" #camel case
+myname = "rizwan" #flat case
+my_name ="rizwan" #snake case
+
+# rules to declare a varibales 
+# 1. varibale name must start with: letter or _ 
+_my_name = "rizwan"
+print(_my_name)
+# myname = "rizwan" 
+
+# 2. Varibale name can contain: letter, numbers, underscore (_)
+my_name1 = "rizwan"
+r1 = 26 
+_1 = 1
+
+# 3. Variables are case sensitive 
+myname = "rizwanali"
+print(myName) 
+
+# 4. variables names can't be a reserved word 
+# for = 10  # for is a reserved word in python  
