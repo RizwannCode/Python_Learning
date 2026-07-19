566ff2a8a3561963087bd96206b13fd39fd4f6ac
Rizwan Ali <rizwanalibhangwar786@gmail.com>
2026-07-19 21:18:04 +0500

    Update first.py

--- chap 1/first.py
+++ chap 1/first.py
@@ -1,8 +1,26 @@
-print("Hello world")
+# print("Hello world")
 
 
+# a=5
+# print(a)
+
+# instructor = 'rishab mishra'
+# print("python by", instructor , sep= '-')
+
+print("Hello World")
 a=5
 print(a)
+tutor='rizwan'
+print("python by",tutor)
 
-instructor = 'rishab mishra'
-print("python by", instructor , sep= '-')
+#Python as a Calculator
+print("5+3")
+print(5+3)
+
+b=10
+c=15
+
+print(b+c)
+print(b/c)
+print(b-c)
+print(b*c)
