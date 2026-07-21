d426bd890b6e6681c75eced57d46e5c3b0de1a6f
Rizwan Ali <rizwanalibhangwar786@gmail.com>
2026-07-21 17:56:29 +0500

    Create assingment1.py

---
+++ assingment1.py
@@ -0,0 +1,11 @@
+
+#Percentage Calculator
+
+Name=input("Enter your Name: ")
+obtained=int(input("Enter your  obtained Marks: "))
+total=int(input("Enter Total Marks: "))
+
+perctange=(obtained/total)*100
+
+print(f"Mr.{Name} you got {perctange}% in your Exams. ")
+
