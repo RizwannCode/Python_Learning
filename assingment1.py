d81e6e620c390f1bfc36468125f159711ec11979
Rizwan Ali <rizwanalibhangwar786@gmail.com>
2026-07-23 18:54:10 +0500

    Update assingment1.py

--- assingment1.py
+++ assingment1.py
@@ -1,21 +1,22 @@
 
-#Percentage Calculator
+# #Percentage Calculator
 
-# Name=input("Enter your Name: ")
-# obtained=int(input("Enter your  obtained Marks: "))
-# total=int(input("Enter Total Marks: "))
+# # Name=input("Enter your Name: ")
+# # obtained=int(input("Enter your  obtained Marks: "))
+# # total=int(input("Enter Total Marks: "))
 
-# perctange=(obtained/total)*100
+# # perctange=(obtained/total)*100
 
-# print(f"Mr.{Name} you got {int(perctange)}% in your Exams. ")
+# # print(f"Mr.{Name} you got {int(perctange)}% in your Exams. ")
 
-print("Student Registration Status")
+# print("Student Registration Status")
 
-user_data={}
+# user_data={}
 
-user_data['Name']=input("Enter your Name: ")
-user_data['Age']=int(input("Enter your Age: "))
-user_data['Height']=float(input("Enter your Height in feets: "))
-user_data['Status']=input("Do you Register for Exams(Yes/No) ")
+# user_data['Name']=input("Enter your Name: ")
+# user_data['Age']=int(input("Enter your Age: "))
+# user_data['Height']=float(input("Enter your Height in feets: "))
+# user_data['Status']=input("Do you Register for Exams(Yes/No) ")
 
-print(f"Your Profile Details \n {user_data}")
+# print(f"Your Profile Details \n {user_data}")
+
